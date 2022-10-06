1065359093b9bae638b57137a5b3dcb11c1961ab
FadwaFouad <fadoalqasem@gmail.com>
2022-10-06 15:55:24 +0200

    add more data

--- app/src/main/java/com/example/tabactivity/SampleData.kt
+++ app/src/main/java/com/example/tabactivity/SampleData.kt
@@ -9,8 +9,13 @@ class SampleFriends {
         Friend("Fadwa Fuad", R.drawable.avatar_girl, "Work", true),
         Friend("Ali Ali", R.drawable.avatar_man3, "Work", false),
         Friend("Mohmaed Abdalla", R.drawable.avatar_man4, "School", true),
-        Friend("Mai Sami", R.drawable.avatar_girl2, "University", false),
-        Friend("Maher Fuad", R.drawable.avatar_man1, "Work", true),
+        Friend("Mai Sami", R.drawable.avatar_girl2, "University", true),
+        Friend("Maher Fuad", R.drawable.avatar_man1, "Work", false),
+        Friend("sameer Hassan", R.drawable.avatar_man4, "School", true),
+        Friend("Aya Yaser", R.drawable.avatar_girl2, "University", true),
+        Friend("Freed Kamal", R.drawable.avatar_man1, "Work", false),
+        Friend("Sara Yaser", R.drawable.avatar_girl2, "University", true),
+        Friend("Izzat Jalal", R.drawable.avatar_man1, "Work", false),
     )
 
     fun getFavFriends(): List<Friend> {
